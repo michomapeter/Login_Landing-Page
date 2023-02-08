c42a5ab5e8ba3b8a48af43c5c2456b56a191b7d5
Peter michoma <pmichoma@kabarak.ac.ke>
2023-02-08 01:17:50 -0500

    creation of the registration api

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const  express = require('express')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
+const User = require('./models/user')
 
-const db ="mongodb+srv://michomapeter_21:michoma123@cluster0.d8jqq.mongodb.net/?retryWrites=true&w=majority"
+const db ="mongodb+srv://michomapeter_21:michoma123@cluster0.d8jqq.mongodb.net/?retryWrites=true&w=eventsdb"
 mongoose.connect(db ,err =>{
     if(err){
         console.error('Error' + err)
@@ -21,6 +22,19 @@ app.use(bodyParser.json())
 app.get('/' ,function(req,res){
 
     res.send('Backend server is running')
+})
+
+app.get('/register' ,(req,res)=>{
+     let userData = req.body
+    let user = new User(userData)
+    user.save((error ,registeredUser)=>{
+        if (error){
+            console.log(error)
+        }
+        else{
+            res.status(200).send(registeredUser)
+        }
+    })
 })
 
 app.listen(PORT ,function(){
